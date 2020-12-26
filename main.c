5e5d268606105b17a666af0fd1a59dfa92a2b584
99003191 <75427938+99003191@users.noreply.github.com>
2020-12-26 17:17:55 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Fibonaaci.h"
+int main() 
+
+{
+    //int n;
+    //printf("Enter the number of terms: ");
+    //scanf("%d", &n);
+    //printf("The Fibonacci Series is: ");
+    fibonaaci(10);
+	return 0;
+   
+}
